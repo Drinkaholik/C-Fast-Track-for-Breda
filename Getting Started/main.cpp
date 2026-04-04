a22b4d061d960367bdc074ac9bb5a6a0eb0c2ce0
Drinkaholik <marlinsquires123@gmail.com>
2026-04-04 21:56:33 +0200

    added project
    using tmpl8 template for 2d rendering and game loop

---
+++ Getting Started/main.cpp
@@ -0,0 +1,27 @@
+
+#include "stdio.h"
+
+
+
+void main()
+{
+	
+	for (int i = 0; i < 10; i++)
+	{
+
+		printf("My name!\n");
+
+	}
+	if (getchar())
+	{
+		for (int i = 0; i < 10; i++)
+		{
+
+			printf("Coding is awesome!\n");
+
+		}
+	}
+
+
+}
+
---
+++ tmpl8-2026/game.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <bitset>
+#include <SDL_scancode.h>
+
+namespace Tmpl8 {
+
+class Surface;
+class Game
+{
+public:
+	void SetTarget( Surface* surface ) { screen = surface; }
+	void Init();
+	void Shutdown();
+	void Tick( float deltaTime );
+	void MouseUp( int button ) { /* implement if you want to detect mouse button presses */ }
+	void MouseDown( int button ) { /* implement if you want to detect mouse button presses */ }
+	void MouseMove( int x, int y ) { /* implement if you want to detect mouse movement */ }
+	void KeyUp(SDL_Scancode key) { keys.reset(key); }
+	void KeyDown(SDL_Scancode key) { keys.set(key); }
+    bool GetKey(SDL_Scancode key) const { return held.test(key); } // returns true if the key is currently held down
+    bool GetKeyPressed(SDL_Scancode key) const { return pressed.test(key); } // returns true if the key was pressed since the last Tick
+    bool GetKeyReleased(SDL_Scancode key) const { return released.test(key); } // returns true if the key was released since the last Tick
+private:
+	Surface* screen;
+    std::bitset<SDL_NUM_SCANCODES> keys; // store key states here
+    std::bitset<SDL_NUM_SCANCODES> held; // store key states from the previous tick here
+    std::bitset<SDL_NUM_SCANCODES> pressed; // store key press events here (set to true on key down, reset to false after processing in Tick)
+    std::bitset<SDL_NUM_SCANCODES> released; // store key release events here (set to true on key up, reset to false after processing in Tick)
+};
+
+}; // namespace Tmpl8

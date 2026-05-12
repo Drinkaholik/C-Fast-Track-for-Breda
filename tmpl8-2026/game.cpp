cf80df365a6e96da7b78070ae992b2e19e59b761
Drinkaholik <marlinsquires123@gmail.com>
2026-05-12 22:48:16 +0200

    i loop from lesson
    causes particles to move around
    
    (written by teacher, not me)

--- tmpl8-2026/game.cpp
+++ tmpl8-2026/game.cpp
@@ -1,5 +1,6 @@
 #include "game.h"
 #include "surface.h"
+#include "template.h"
 
 #include <cstdio> //printf
 #include <iostream>
@@ -9,57 +10,24 @@ namespace Tmpl8
     // -----------------------------------------------------------
     // Initialize the application
     // -----------------------------------------------------------
+    int x[4096], y[4096];
+
     void Game::Init()
     {
+        for (int i = 0; i < 4096; i++)
+            x[i] = IRand(800), y[i] = IRand(512);
     }
 
-    // -----------------------------------------------------------
-    // Close down application
-    // -----------------------------------------------------------
-    void Game::Shutdown()
-    {
-    }
+    void Game::Shutdown() {}
 
-    static Sprite rotatingGun(new Surface("assets/aagun.tga"), 36);
-    static int frame = 0;
-
-    // -----------------------------------------------------------
-    // Main application tick function
-    // -----------------------------------------------------------
     void Game::Tick(float deltaTime)
     {
-        // Update key states
-        pressed = keys & ~held; // keys that are currently down but were not down in the previous tick
-        released = ~keys & held; // keys that were down in the previous tick but are not down now
-        held = keys; // update prevKeys for the next tick
-
-        // Handle input.
-        if (GetKey(SDL_SCANCODE_W))
-        {
-            std::cout << "W" << std::endl;
-        }
-        if (GetKey(SDL_SCANCODE_A))
-        {
-            std::cout << "A" << std::endl;
-        }
-        if (GetKey(SDL_SCANCODE_S))
-        {
-            std::cout << "S" << std::endl;
-        }
-        if (GetKey(SDL_SCANCODE_D))
-        {
-            std::cout << "D" << std::endl;
-        }
-
-        // clear the graphics window
         screen->Clear(0);
-        // print something in the graphics window
-        screen->Print("hello world", 2, 2, 0xffffff);
-        // print something to the text window
-        // printf("this goes to the console window.\n");
-        // draw a sprite
-        rotatingGun.SetFrame(frame);
-        rotatingGun.Draw(screen, 100, 100);
-        if (++frame == 36) frame = 0;
+        for (int i = 0; i < 4096; i++)
+        {
+            x[i] = (x[i] + 800 + (((i & 1) * 2) - 1)) % 800;
+            y[i] = (y[i] + 512 + ((((i >> 2) & 1) * 2) - 1)) & 511;
+            screen->GetBuffer()[x[i] + y[i] * 800] = 0xffffff;
+        }
     }
 };

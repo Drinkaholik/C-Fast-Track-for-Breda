c645a8368b1d335dd88359d0c90230fbfc480e0b
Drinkaholik <marlinsquires123@gmail.com>
2026-05-13 22:39:32 +0200

    moved playerHP functions to header file
    they were tiny so they're better off in the header - reduce cache misses

--- tmpl8-2026/tmpl8/playerHP.h
+++ tmpl8-2026/tmpl8/playerHP.h
@@ -6,18 +6,35 @@
 class PlayerHP : public Component
 {
 
-public:
-
-	PlayerHP(int maxHP) : maxHP(maxHP), hp(maxHP) {};
-
-	void SetHP(int hp);
-	int GetHP();
-	
-
 private:
 
 	int hp;
 	int maxHP;
+
+
+public:
+
+	PlayerHP(int maxHP) : maxHP(maxHP), hp(maxHP) {};
+
+
+	// These functions are small enough to keep in the header
+	// Should give better performance since it'll stop cache misses
+	void SetHP(int value)
+	{
+		value = Clamp(hp, 0, maxHP);
+
+		hp = value;
+	}
+
+	void IncreaseHP(int value) {hp += value;};
+
+	void DecreaseHP(int value) {hp -= value;};
+
+
+
+	int GetHP() {return hp;}
+	
+
 
 
 };
